01838b1571f8be18a2d6f5b65a713319d8cc7aae
dotuananh <dotuananh220201@gmail.com>
2022-08-27 09:18:03 +0700

    Header ui #6

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,8 +1,23 @@
 import { useState } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons'
-import Tippy from '@tippyjs/react/headless'
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+    faCloudUpload,
+    faUser,
+    faCoins,
+    faGear,
+    faSignOut,
+} from '@fortawesome/free-solid-svg-icons'
+import Tippy from '@tippyjs/react'
+import HeadlessTippy from '@tippyjs/react/headless'
+import 'tippy.js/dist/tippy.css'
 
 import Button from '~/components/Button'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
@@ -52,16 +67,43 @@ const MENU_ITEMS = [
 function Header() {
     const [searchResult, setSearchResult] = useState([])
 
+    const currentUser = true
+
     // Handle logic
     const handleMenuChange = (menuItem) => {
-        switch(menuItem.type) {
+        switch (menuItem.type) {
             case 'language':
                 // Handle change language
                 break
             default:
-                
+
         }
     }
+
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@tiger050794',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ]
 
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
@@ -69,7 +111,7 @@ function Header() {
                 <img src={images.logo} alt='TikTok' />
             </div>
 
-            <Tippy
+            <HeadlessTippy
                 interactive
                 visible={searchResult.length > 0}
                 // placement='bottom-start'
@@ -98,22 +140,48 @@ function Header() {
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                 </div>
-            </Tippy>
-            <div className={cx('action')}>
-                <Button text>Upload</Button>
-                <Button primary>Log in</Button>
+            </HeadlessTippy>
+
+            <div className={cx('actions')}>
+                {currentUser ? (
+                    <>
+                        <Tippy
+                            delay={200}
+                            content="Upload video" placement='bottom'>
+                            <button className={cx('action-btn')}>
+                                <FontAwesomeIcon icon={faCloudUpload} />
+                            </button>
+                        </Tippy>
+                    </>
+                ) : (
+                    <>
+                        <Button text>Upload</Button>
+                        <Button primary>Log in</Button>
+                    </>
+                )}
+
 
                 <Menu
-                    items={MENU_ITEMS}
+                    items={currentUser ? userMenu : MENU_ITEMS}
                     onChange={handleMenuChange}
                 >
-                    <button className={cx('more-btn')}>
-                        <FontAwesomeIcon icon={faEllipsisVertical} />
-                    </button>
+                    {currentUser ? (
+                        <img
+                            className={cx('user-avatar')}
+                            src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/115cca71265c94522079ac7e93a0274b.jpeg?x-expires=1661324400&x-signature=VupGkvaMD7tKbZbOt5TBOqJXJWA%3D"
+                            alt="sontungmtp"
+                        />
+                    ) : (
+                        <>
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        </>
+                    )}
                 </Menu>
             </div>
-        </div>
-    </header>
+        </div >
+    </header >
 }
 
 export default Header
