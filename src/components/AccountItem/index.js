7f1fc1bf57bbf3e7ee3787b32a49711e0f539019
dotuananh <dotuananh220201@gmail.com>
2022-08-27 16:20:54 +0700

    Logic header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind'
+import Image from '~/components/Image'
 import styles from './AccountItem.module.scss'
 
 const cx = classNames.bind(styles)
@@ -8,7 +9,11 @@ const cx = classNames.bind(styles)
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/115cca71265c94522079ac7e93a0274b.jpeg?x-expires=1661324400&x-signature=VupGkvaMD7tKbZbOt5TBOqJXJWA%3D" alt="sontungmtp" />
+            <Image
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/115cca71265c94522079ac7e93a0274b.jpeg?x-expires=1661324400&x-signature=VupGkvaMD7tKbZbOt5TBOqJXJWA%3D"
+                alt="sontungmtp"
+            />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Son Tung M-TP</span>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faCircleXmark,
-    faSpinner,
     faEllipsisVertical,
     faEarthAsia,
     faCircleQuestion,
@@ -14,17 +11,15 @@ import {
     faSignOut,
 } from '@fortawesome/free-solid-svg-icons'
 import Tippy from '@tippyjs/react'
-import HeadlessTippy from '@tippyjs/react/headless'
 import 'tippy.js/dist/tippy.css'
 
 import Button from '~/components/Button'
-import { Wrapper as PopperWrapper } from '~/components/Popper'
 import styles from './Header.module.scss'
 import images from '~/assets/images'
-import AccountItem from '~/components/AccountItem'
 import Menu from '~/components/Popper/Menu'
-import { InboxIcon, MessageIcon, SearchIcon, UploadIcon } from '~/components/Icons'
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons'
 import Image from '~/components/Image'
+import Search from '../Search'
 
 const cx = classNames.bind(styles)
 
@@ -65,8 +60,6 @@ const MENU_ITEMS = [
 ]
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([])
-
     const currentUser = true
 
     // Handle logic
@@ -111,36 +104,7 @@ function Header() {
                 <img src={images.logo} alt='TikTok' />
             </div>
 
-            <HeadlessTippy
-                interactive
-                visible={searchResult.length > 0}
-                // placement='bottom-start'
-                render={attrs => (
-                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                        <PopperWrapper>
-                            <h4 className={cx('search-title')}>
-                                Accounts
-                            </h4>
-                            <AccountItem />
-                            <AccountItem />
-                            <AccountItem />
-                            <AccountItem />
-                        </PopperWrapper>
-                    </div>
-                )}
-            >
-                <div className={cx('search')}>
-                    <input placeholder='Search accounts and videos' spellCheck={false} />
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-
-                    <button className={cx('search-btn')}>
-                        <SearchIcon />
-                    </button>
-                </div>
-            </HeadlessTippy>
+            <Search />
 
             <div className={cx('actions')}>
                 {currentUser ? (
